710b2a68b09794b8284847aeb6df83ab6f6ebd22
Rajeshwari Vakharia <rashu.cloud11@gmail.com>
2025-06-20 19:26:18 +0530

    DSA in GO

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import dsa "dsa_go/dsa_go"
+import (
+	dsa "dsa_go/dsa_go"
+	"fmt"
+)
 
 func main() {
+	fmt.Println("\nLinkedList")
 	dsa.ImplementOps()
+
+	fmt.Println("\nQueue")
 	dsa.ImplementQueue()
+
+	fmt.Println("\nStack")
 	dsa.ImplementStack()
+
+	fmt.Println("\nTree")
+	dsa.ImplementTree()
 }
